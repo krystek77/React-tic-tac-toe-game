6bbad5da99dff3981e1c199a5a0819fee6f162cd
krystek77 <krystian.wrona77@gmail.com>
2019-05-26 10:28:37 +0200

    Updates

---
+++ precache-manifest.87ff8cf43a31c03ada6fb6e32c8209a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e71671b384d4c3f235afe6d578cfcb80",
+    "url": "/React-tic-tac-toe-game/static/media/battle.e71671b3.wav"
+  },
+  {
+    "revision": "ef9109a20fd4721f3526",
+    "url": "/React-tic-tac-toe-game/static/js/runtime~main.14c92a03.js"
+  },
+  {
+    "revision": "eae6f5bf6cc19a2e8c04",
+    "url": "/React-tic-tac-toe-game/static/js/main.80cd28bf.chunk.js"
+  },
+  {
+    "revision": "aaa1a6efcfcb18de2137",
+    "url": "/React-tic-tac-toe-game/static/js/2.411d1eb4.chunk.js"
+  },
+  {
+    "revision": "eae6f5bf6cc19a2e8c04",
+    "url": "/React-tic-tac-toe-game/static/css/main.8c303286.chunk.css"
+  },
+  {
+    "revision": "2179722ebb88ead655718443f790294d",
+    "url": "/React-tic-tac-toe-game/index.html"
+  }
+];
